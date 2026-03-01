c9cf0f1330d72475afb523e4ce2d2db2f383b706
Dimitar Ivanov <dimitucha2004@gmail.com>
2026-03-01 21:41:59 +0200

    Implement the hadamard gate functionality

--- backend/main.py
+++ backend/main.py
@@ -17,6 +17,8 @@ app.add_middleware(
 
 X_GATE = np.array([[0, 1], [1, 0]])
 
+H_GATE = (1 / np.sqrt(2)) * np.array([[1, 1], [1, -1]])
+
 @app.get("/")
 def home():
     return {"message": "Quantum Simulator API is running"}
@@ -26,3 +28,10 @@ def apply_x(data: QubitState):
     vec = np.array(data.state)
     new_state = np.dot(X_GATE, vec)
     return {"new_state": new_state.tolist()}
+
+@app.post("/apply-h")
+def apply_h(data: QubitState):
+    vec = np.array(data.state)
+    new_state = np.dot(H_GATE, vec)
+    return{"new_state": new_state.tolist()}
+
